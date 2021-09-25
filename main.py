e3d2ef4d343bce1e2651c2435dd601e5f58281ff
gprettyman <90404415+gprettyman@users.noreply.github.com>
2021-09-25 11:43:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pybricks.ev3devices import (Motor, ColorSensor, InfraredSensor)
 from pybricks.parameters import (Port, Direction, Color)
 from pybricks.robotics import DriveBase
 
+# Initialize robot and components
 greg = EV3Brick()
 colorBlind = ColorSensor(Port.S1)
 predatorEyes = InfraredSensor(Port.S4)
@@ -14,15 +15,17 @@ wheels = DriveBase(leftMotor, rightMotor, wheel_diameter=43.2, axle_track = 145)
 attackMotor = Motor(Port.D, positive_direction = Direction.CLOCKWISE, gears = None)
 
 
-
+# Creates a counter (so the program doesn't run forever)
 Quit = False
 counter = 0
 
+# If it crosses the red lines (around the arena) or is too close to another robot or wall, backs up
 def retreat():
     wheels.stop()
     wheels.turn(120)
     greg.speaker.beep(frequency = 2500, duration = 500)
-        
+
+# After seeing another robot marked with Black, attacks
 def attack():
     greg.speaker.beep(frequency = 3000, duration = 250)
     attackMotor.run_angle(20, 120, then=Stop.HOLD, wait=True)
@@ -30,8 +33,6 @@ def attack():
 
 while Quit != True:
     wheels.drive(300, 0)
-    if counter == 10:
-        Quit = True
     if (colorBlind.color() == Color.RED) or (predatorEyes.distance() < 25):
         retreat()
         counter += 1
